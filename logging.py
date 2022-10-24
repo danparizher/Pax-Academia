aca4de388327adc37b4c10a53b5ea5dbd9b00ab7
Arborym <dparizher@gmail.com>
2022-10-24 15:38:31 -0400

    type hint

--- logging.py
+++ logging.py
@@ -6,5 +6,5 @@ class log():
     def __init__(self,message) -> None:
         with open("data\\log.txt", "a") as log:
             now = datetime.datetime.now()
-            now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+            now_str: str = now.strftime("%Y-%m-%d %H:%M:%S")
             log.write(f"{now_str} - {message}\n")
