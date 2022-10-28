418dc7f4c7ee1a4cffc200db8a3ff1de2538ebf0
Arborym <dparizher@gmail.com>
2022-10-28 11:56:51 -0400

    Update footers and links

--- EmbedBuilder.py
+++ EmbedBuilder.py
@@ -8,13 +8,14 @@ class EmbedBuilder:
         description,
         color=0xFF7900,
         image=None,
-        thumbnail="https://pbs.twimg.com/profile_images/988662835180797952/9fWyq5hr_400x400.jpg",
+        thumbnail=None,
     ) -> None:
         self.title: str | None = title
         self.description: str | None = description
         self.color: int = color
         self.thumbnail: str | None = thumbnail
         self.footer: str = "Powered by Homework Help"
+        self.icon_url: str = "https://pbs.twimg.com/profile_images/988662835180797952/9fWyq5hr_400x400.jpg"
         self.image: str | None = image
 
     def build(self) -> discord.Embed:
@@ -25,5 +26,5 @@ class EmbedBuilder:
             embed.set_image(url=self.image)
         if self.thumbnail:
             embed.set_thumbnail(url=self.thumbnail)
-        embed.set_footer(text=self.footer)
+        embed.set_footer(text=self.footer, icon_url=self.icon_url)
         return embed
