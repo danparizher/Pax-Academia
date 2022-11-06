2b91040ba8c9ac6ba26fa9102656b098d7c4e190
Arborym <dparizher@gmail.com>
2022-11-05 20:01:15 -0400

    work on alerts

--- cogs/Alerts.py
+++ cogs/Alerts.py
@@ -2,12 +2,13 @@ import re
 import sqlite3
 
 import discord
+from discord.commands import Option
 from discord.ext import commands
 
 from util.EmbedBuilder import EmbedBuilder
 from util.Logging import log
 
-# TODO: Add a RegEx implementation.
+
 class Alerts(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
@@ -17,6 +18,18 @@ class Alerts(commands.Cog):
             "CREATE TABLE IF NOT EXISTS alerts (keyword TEXT, user_id INTEGER, author_name TEXT)"
         )
         self.db.commit()
+
+    def get_keywords(self, db, user_id, author_name) -> list:
+        c = db.cursor()
+        c.execute(
+            "SELECT keyword FROM alerts WHERE user_id = ? AND author_name = ?",
+            (user_id, author_name),
+        )
+        keywords = c.fetchall()
+        choices = []
+        for keyword in keywords:
+            choices.append(str(name=keyword[0], value=keyword[0]))
+        return choices
 
     # Allows the user to enter a keyword to be alerted when it is mentioned in the guild. When the keyword is used, the bot will send a DM to the user.
     @commands.slash_command(
@@ -57,7 +70,17 @@ class Alerts(commands.Cog):
         name="remove-alert",
         description="Removes an alert for a keyword.",
     )
-    async def remove_alert(self, ctx: commands.Context, keyword: str) -> None:
+    async def remove_alert(
+        self,
+        ctx: commands.Context,
+        keyword: Option(
+            str,
+            "The keyword you want to remove",
+            choices=self.get_keywords(self.db, ctx.author.id, ctx.author.name),
+            required=True,
+        ),
+    ) -> None:
+
         # Check if the keyword is in the database.
         c = self.db.cursor()
         c.execute(
