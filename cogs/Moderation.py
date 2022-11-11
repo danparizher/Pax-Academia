b3c7a3fa9d78d95efd64716d446a33b87f681570
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2022-11-11 12:37:03 -0800

    add `MessageFingerprint.matches`

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -3,6 +3,7 @@ from typing import TypeAlias, Optional
 from hashlib import sha256
 import urllib.request
 import aiohttp
+import re
 
 import discord
 from discord.commands import option
@@ -18,10 +19,25 @@ Hash: TypeAlias = bytes
 class MessageFingerprint:
     author_id: discord.Snowflake
     channel_id: discord.Snowflake
-    content_hash: Hash  # the hash
 
     attachment_urls: list[str]  # the discord content URLs for each of the message's uploaded attachments
-    _cached_attachment_hashes: Optional[set[Hash]] = None  # populated on the first call to `get_attachment_hashes`
+    cached_attachment_hashes: Optional[set[Hash]] = None  # populated on the first call to `get_attachment_hashes`
+
+    content_hash: Optional[
+        Hash
+    ] = None  # hash of the message body (with whitespace removed), or None if there is no message body
+
+    # shortcut to build a fingerprint given a message
+    @classmethod
+    def build(cls, message: discord.Message) -> "MessageFingerprint":
+
+        content_without_whitespace = re.sub(r"\s", "", message.content)
+        return cls(
+            author_id=message.author.id,
+            channel_id=message.channel.id,
+            attachment_urls=[attachment.url for attachment in message.attachments],
+            content_hash=cls.hash(content_without_whitespace) if content_without_whitespace else None,
+        )
 
     # performs a SHA256 hash
     @staticmethod
@@ -31,12 +47,15 @@ class MessageFingerprint:
 
         return sha256(data).digest()
 
+    # retrieves and caches attachment hashes
+    # the first call will actually download every attachment,
+    # and all subsequent calls will just return the cached values
     async def get_attachment_hashes(self) -> set[Hash]:
-        if self._cached_attachment_hashes is None:
-            self._cached_attachment_hashes = set()
+        if self.cached_attachment_hashes is None:
+            self.cached_attachment_hashes = set()
 
             async with aiohttp.ClientSession() as session:
-                for attachment_url in self.attachment_urls:
+                for attachment_url in self.attachment_urls[:5]:  # only load first 5 attachments to prevent abuse
                     try:
                         async with session.get(attachment_url) as resp:
                             attachment_data = await resp.read()
@@ -46,9 +65,25 @@ class MessageFingerprint:
                         # so it's fine to just silently drop any errors
                         continue
 
-                    self._cached_attachment_hashes.add(self.hash(attachment_data))
+                    self.cached_attachment_hashes.add(self.hash(attachment_data))
 
-        return self._cached_attachment_hashes
+        return self.cached_attachment_hashes
+
+    # returns True if two message fingerprints are similar (i.e. if the message is a 'multipost')
+    # specifically, if at least one of the attachments are identical or if the message body
+    # is identical and not blank
+    async def matches(self, other: "MessageFingerprint") -> bool:
+        # different authors cannot have matching fingerprints
+        if self.author_id != other.author_id:
+            return False
+
+        # if there is content and it matches, then the fingerprint matches
+        if self.content_hash is not None and (self.content_hash == other.content_hash):
+            return True
+
+        # otherwise, at least one of the attachments must match
+        matching_attachments = await self.get_attachment_hashes() & await other.get_attachment_hashes()
+        return len(matching_attachments) > 0
 
 
 # TODO: Incomplete/in testing. Do not add to cog list.
