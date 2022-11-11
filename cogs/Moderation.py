582d4ecf5059d2ad31d9c0d238c4489805a7f77c
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2022-11-11 12:16:37 -0800

    add MessageFingerprint

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1,8 +1,51 @@
+from dataclasses import dataclass
+from typing import TypeAlias, Optional
+from hashlib import sha256
+import urllib.request
+
 import discord
 from discord.commands import option
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 from util.EmbedBuilder import EmbedBuilder
+
+# hashlib just returns a bytes object
+Hash: TypeAlias = bytes
+
+# this is a simple structure that stores important information
+@dataclass
+class MessageFingerprint:
+    author_id: discord.Snowflake
+    channel_id: discord.Snowflake
+    content_hash: Hash  # the hash
+
+    attachment_urls: list[str]  # the discord content URLs for each of the message's uploaded attachments
+    _cached_attachment_hashes: Optional[set[Hash]] = None  # populated on the first call to `get_attachment_hashes`
+
+    # performs a SHA256 hash
+    @staticmethod
+    def hash(data: str | bytes) -> Hash:
+        if isinstance(data, str):
+            data = data.encode()
+
+        return sha256(data).digest()
+
+    def get_attachment_hashes(self) -> set[Hash]:
+        if self._cached_attachment_hashes is None:
+            self._cached_attachment_hashes = set()
+
+            for attachment_url in self.attachment_urls:
+                try:
+                    response = urllib.request.urlopen(attachment_url)
+                except:
+                    # it's possible that Discord may have deleted the attachment, and so we would get a 404
+                    # furthermore, it's not super important that this function is 100% perfectly accurate
+                    # so it's fine to just silently drop any errors
+                    continue
+
+                self._cached_attachment_hashes.add(self.hash(response.read()))
+
+        return self._cached_attachment_hashes
 
 
 # TODO: Incomplete/in testing. Do not add to cog list.
@@ -10,9 +53,7 @@ class Moderation(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @commands.slash_command(
-        name="mkick", description="Kicks multiple users from the server."
-    )
+    @commands.slash_command(name="mkick", description="Kicks multiple users from the server.")
     @commands.has_permissions(kick_members=True)
     async def mkick(
         self,
