9c92e799a57fdf432b6102c1ce26efa465f669fe
Arborym <dparizher@gmail.com>
2022-11-12 09:34:09 -0500

    Fix forbidden error

--- cogs/Surveys.py
+++ cogs/Surveys.py
@@ -16,24 +16,38 @@ def surveys() -> list[str]:
 
 
 class Surveys(commands.Cog):
-    def __init__(self, bot) -> None:
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
+        """
+        If the message is not in the "surveys" channel, the author does not have the "Staff" role, and
+        the author is not a bot, then check if the message contains a survey link. If it does, send a
+        message to the author telling them to post survey links in the "surveys" channel.
+        
+        :param message: The message object that triggered the event
+        :type message: discord.Message
+        """
         try:
-            if message.channel.name != "surveys" and "276969339901444096" not in [
-                role.id for role in message.author.roles
-            ]:
+            if (
+                message.channel.name != "surveys"
+                and "276969339901444096"
+                not in [role.id for role in message.author.roles]
+                and not message.author.bot
+            ):
                 for survey in surveys():
                     if survey in message.content:
                         embed = EmbedBuilder(
                             title="Survey Link Detected",
                             description=f"Hey {message.author.mention}, it looks like you tried to post a survey link. If this is correct, please post survey links in the <#580936851360055296> channel instead! Thanks.",
                         ).build()
-                        await message.channel.send(
-                            content=f"<@{message.author.id}>", embed=embed
-                        )
+                        try:
+                            await message.channel.send(
+                                content=f"<@{message.author.id}>", embed=embed
+                            )
+                        except discord.errors.Forbidden:
+                            pass
         except AttributeError:
             pass
         await self.bot.process_commands(message)
