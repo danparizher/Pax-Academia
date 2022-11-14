8fb42b3736819d819e54c3b33b3b1a97a5409431
Arborym <dparizher@gmail.com>
2022-11-14 18:59:13 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 Rem !SKA#0001 24/10/2022
-Rem This program is used to create and setup the bot and it's virtual environment
+Rem This program is used to create and setup the bot and its virtual environment
 Rem Only run this program if you have python 3.10 on PATH
 
 Rem create virtual environment
