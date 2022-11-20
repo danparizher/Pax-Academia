c003711f6c029d5b1a1bd09a8b416d22e5239a4f
Arborym <dparizher@gmail.com>
2022-11-20 14:21:06 -0500

    add docs

--- util/bandwidth.py
+++ util/bandwidth.py
@@ -1,6 +1,6 @@
-from typing import Literal
 import sqlite3
 from datetime import datetime, timedelta
+from typing import Literal
 
 import humanize
 
@@ -31,6 +31,14 @@ def log(
 
 # returns information about bandwidth after the given date
 def summary(after: datetime) -> dict[str, dict[str, int | float]]:
+    """
+    It returns a dictionary of dictionaries, where the keys of the outer dictionary are categories, and
+    the keys of the inner dictionaries are "inbound_bytes", "outbound_bytes", and "usages"
+    
+    :param after: datetime
+    :type after: datetime
+    :return: A dictionary of dictionaries.
+    """
     cursor = database.cursor()
 
     query = """
@@ -54,6 +62,12 @@ def summary(after: datetime) -> dict[str, dict[str, int | float]]:
 
 # Pretty print bandwidth information after a certain data (humans should manually invoke this function!)
 def print_summary(after: datetime) -> None:
+    """
+    It prints a summary of the bandwidth usage since the given time
+    
+    :param after: The datetime object to start the summary from
+    :type after: datetime
+    """
     print(f"Bandwidth Summary (from {after.strftime('%A %b %d %H:%M')} to now)")
     for category, info in summary(after).items():
         print(f"* Within the {category!r} category")
@@ -64,7 +78,10 @@ def print_summary(after: datetime) -> None:
 
 
 # prints the summary from the last week
-def print_weekly_summary():
+def print_weekly_summary() -> None:
+    """
+    It prints a summary of the last 7 days
+    """
     print_summary(datetime.now() - timedelta(days=7))
 
 
