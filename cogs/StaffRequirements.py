49a82ab68be58a924065c32378f87e193cdb5622
Redwood <dccntct@posteo.de>
2022-11-21 14:06:09 +0100

    The result of checking staff requirements is now displayed in a single message.

--- cogs/StaffRequirements.py
+++ cogs/StaffRequirements.py
@@ -36,37 +36,33 @@ class StaffRequirement(commands.Cog):
         joined_days_ago = (
             datetime.now(tz=account.joined_at.tzinfo) - account.joined_at
         ).days % 365
-
+        
+        content=[
+            [
+                "Account Age",
+                f"**{created_years_ago}** year{'s' if created_years_ago != 1 else ''}, **{created_days_ago}** day{'s' if created_days_ago != 1 else ''}",
+                True,
+            ],
+            [
+                "Joined",
+                f"**{joined_years_ago}** year{'s' if joined_years_ago != 1 else ''}, **{joined_days_ago}** day{'s' if joined_days_ago != 1 else ''} ago",
+                True,
+            ]
+        ]
+        if (created_years_ago >= 1) and (joined_days_ago >= 30):
+            content.append([
+                f"Congratulations, {account.mention}! You meet the basic requirements for staff. Please fill out the application form [here](https://goo.gl/forms/Z3mVQwLdiNZcKHx52)."
+                ])
+        else:
+            content.append([
+                f"Unfortunately, you do not meet the basic requirements in order to apply for staff. Your account must be at least 1 year old and you must have been a member of the server for at least 30 days."
+                ])
         embed = EmbedBuilder(
             title="User info",
             description=f"{account.mention} was created on {account.created_at.strftime('%B %d, %Y')} and joined on {account.joined_at.strftime('%B %d, %Y')}.",
-            fields=[
-                [
-                    "Account Age",
-                    f"**{created_years_ago}** year{'s' if created_years_ago != 1 else ''}, **{created_days_ago}** day{'s' if created_days_ago != 1 else ''}",
-                    True,
-                ],
-                [
-                    "Joined",
-                    f"**{joined_years_ago}** year{'s' if joined_years_ago != 1 else ''}, **{joined_days_ago}** day{'s' if joined_days_ago != 1 else ''} ago",
-                    True,
-                ],
-            ],
+            fields=content,
         ).build()
         await ctx.respond(embed=embed, ephemeral=True)
-
-        if (created_years_ago >= 1) and (joined_days_ago >= 30):
-            embed = EmbedBuilder(
-                title="Application Link",
-                description=f"Congratulations, {account.mention}! You meet the basic requirements for staff. Please fill out the application form [here](https://goo.gl/forms/Z3mVQwLdiNZcKHx52).",
-            ).build()
-            await ctx.respond(embed=embed, ephemeral=True)
-        else:
-            embed = EmbedBuilder(
-                title="Missing Requirements",
-                description=f"Unfortunately, you do not meet the basic requirements in order to apply for staff. Your account must be at least 1 year old and you must have been a member of the server for at least 30 days.",
-            ).build()
-            await ctx.respond(embed=embed, ephemeral=True)
 
         log(f"User {account} checked their requirements in {ctx.guild}.")
 
