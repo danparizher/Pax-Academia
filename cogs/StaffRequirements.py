5b539ac7408cfc9e1b55f49691177d6773673484
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2022-12-17 02:32:22 +0100

    3am work done

--- cogs/StaffRequirements.py
+++ cogs/StaffRequirements.py
@@ -5,6 +5,8 @@ import discord.ui
 from discord.ext import commands
 from humanize import precisedelta
 
+import sqlite3
+
 from util.EmbedBuilder import EmbedBuilder
 from util.Logging import log
 
@@ -20,11 +22,17 @@ class StaffAppView(discord.ui.View):
 class StaffRequirement(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.db= sqlite3.connect("util/database.sqlite")
+        self.cursor = self.db.cursor()
+
+    def fetch_message_count(self, uid):
+        self.cursor.execute("SELECT amount FROM messagecount WHERE uid = ?", (uid,))
+        return self.cursor.fetchone()[0]
 
     # Allows the user to set the keeptime to a value other than the default
     @commands.slash_command(
-        name="check-requirements",
-        description="Check if the account meets requirements for staff.",
+        name="apply-for-staff",
+        description="Apply for a staff position if your account meets the requirements!",
     )
     async def checkreqs(self, ctx: commands.Context) -> None:
         """
@@ -33,6 +41,7 @@ class StaffRequirement(commands.Cog):
         :param ctx: commands.Context
         :type ctx: commands.Context
         """
+        msg_amount = self.fetch_message_count(ctx.author.id)
         account = ctx.author
 
         time_since_creation = (
@@ -53,6 +62,11 @@ class StaffRequirement(commands.Cog):
                 {precisedelta(time_since_join, minimum_unit='days', format="%.0F")} ago""",
                 True,
             ],
+            [
+                "Messages",
+                f"{msg_amount} messages",
+                True
+            ]
         ]
 
         # if staff requirements are met
@@ -60,7 +74,7 @@ class StaffRequirement(commands.Cog):
         # time since join is at least 30 days
         if time_since_creation >= timedelta(weeks=52) and time_since_join >= timedelta(
             days=30
-        ):
+        ) and msg_amount >= 500:
             embed = EmbedBuilder(
                 title="Congratulations!",
                 description="You meet the basic requirements for staff. You may apply for staff below.",
@@ -77,9 +91,10 @@ class StaffRequirement(commands.Cog):
             embed = EmbedBuilder(
                 title="Missing Requirements",
                 description=f"""Unfortunately, you do not meet the basic requirements in order to apply for staff.
-                Your account must be at least 1 year old and you must have been a member of the server for at least 30 days.
-
-                You will be eligible to apply for staff in **{precisedelta(wait_time, minimum_unit='days', format="%.0F")}**.""",
+                Your account must be at least 1 year old, you must have been a member of the server for at least 
+                30 days and you need to have at least 500 messages sent totalin any channel.""" + 
+                f"""You will be eligible to apply for staff in **{precisedelta(wait_time, minimum_unit='days', format="%.0F")}**.""" if msg_amount > 500 else "" +
+                f"""You need to send **{(left := 500 - msg_amount)}** more message{"" if left == 1 else "s"}.""" if msg_amount < 500 else "",
                 fields=fields,
                 color=0xFF0000,  # RED
             ).build()
