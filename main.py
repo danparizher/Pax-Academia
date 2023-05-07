49e4671cc8f7693608b5aaa02521aa15451fb8b2
Arborym <dparizher@gmail.com>
2023-05-07 15:18:04 -0400

    re-add db builder

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# Creates the db if it doesn't exist
+import util.db_builder
+
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 discord_intents = discord.Intents.default()
