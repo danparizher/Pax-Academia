86dc807087a8e52c92cd1d89d0de472a18f65a47
Sebastiaan Daniels <79920562+sebastiaan-daniels@users.noreply.github.com>
2023-06-27 01:20:32 +0200

    Created limit wrapper
    This wrapper can be use UNDERNEATH application command wrappers to set a limit for users.

--- util/Logging.py
+++ util/Logging.py
@@ -3,6 +3,57 @@
 import datetime
 import discord
 import re
+import sqlite3
+import functools
+from util.EmbedBuilder import EmbedBuilder
+
+
+def limit(_limit_level: int) -> callable:
+    """
+    Limit levels:
+    None: No limit
+    1: Cannot use database commands (I.e alerts & applications)
+    2: Cannot use pax non help specific commands (I.e. chemsearch, define, wiki, ...)
+    3: Cannot use pax. (ping, rule, tip...)
+    Levels are inclusive, so if you are level 2, you cannot use level 1 commands either.
+    """
+
+    def decorator(func: callable) -> callable:
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs) -> callable:
+            """
+            Wrapper to determine if the user is limited or not.
+            """
+
+            # Get the author id
+            author_id = None
+            for arg in args:
+                try:
+                    author_id = arg.author.id
+                    ctx = arg
+                except AttributeError:  # Type is not a context
+                    continue
+
+            if author_id is not None:  # if no author exists, then we cannot limit, return func instead
+                conn = sqlite3.connect("util/database.sqlite")
+                c = conn.cursor()
+                limit_level = c.execute("SELECT limitLevel from user where uid = ?", (author_id,)).fetchone()[
+                    0] or 0  # if no limit level exists, then it is 0
+                if limit_level >= _limit_level:
+                    embed = EmbedBuilder(
+                        title="You cannot use this command!",
+                        description="You are limited from using this command! Please contact a moderator if you believe this is a mistake.",
+                        color=0xFF0000,
+                    ).build()
+                    await ctx.respond(embed=embed, ephemeral=True)
+                    Log(
+                        f"$ tried to use {ctx.command.name}. But is limited from using it.", ctx.author)
+                    return
+
+            result = await func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
 
 
 class Log:
