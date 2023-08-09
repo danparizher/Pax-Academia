1869acdc755fbbb5f523ea9f70ee8a0e11f3881e
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2023-08-08 17:51:37 -0700

    improve python detection

--- util/code_detection/python.py
+++ util/code_detection/python.py
@@ -17,6 +17,7 @@ LINE_STARTING_KEYWORDS = (
     "raise",
     "return",
     "yield",
+    "#",
 )
 
 
@@ -25,10 +26,17 @@ class PythonDetector(DetectorBase):
     def language(self) -> str:
         return "python"
 
+    @property
+    def min_plain_text_lines_in_a_row(self) -> int:
+        # Can reduce this to 1 for Python
+        # since we're pretty confident that `line_is_probably_code` will
+        # match every feasible line of python code
+        return 1
+
     def line_is_probably_code(self, line: str) -> bool:
         return (
             (line.startswith(("  ", "\t")) and not line.isspace())
-            or line.rstrip().endswith((":", ")", ","))
+            or line.rstrip().endswith((":", "(", ")", "[", "]", "{", "}", ","))
             or line.lstrip().startswith(LINE_STARTING_KEYWORDS)
             or re.match(r"\s*[a-zA-Z_][a-zA-Z_0-9]*\s*=", line) is not None
         )
