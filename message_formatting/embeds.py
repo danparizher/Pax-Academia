941ed996e52e733d6e849219f68f56e956d60270
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2023-08-19 03:58:13 -0700

    improve message_formatting/embeds.py typing with covariance

--- message_formatting/embeds.py
+++ message_formatting/embeds.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+
 import discord
 
 
@@ -8,7 +10,7 @@ class EmbedBuilder:
         description: str | None = None,
         color: int = 0xFF7900,
         image: str | None = None,
-        fields: list[tuple[str, str, bool]] | None = None,
+        fields: Sequence[tuple[str, str, bool]] | None = None,
         thumbnail: str | None = None,
         footer: str = "Powered by Homework Help",
         icon_url: str = "https://pbs.twimg.com/profile_images/988662835180797952/9fWyq5hr_400x400.jpg",
