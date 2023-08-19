9117d71f5284196950f626172666a921f4b6235a
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2023-08-19 03:03:16 -0700

    fix typing errors in util/limiter.py

--- util/limiter.py
+++ util/limiter.py
@@ -1,12 +1,24 @@
 import functools
+from typing import Awaitable, Callable, ParamSpec, TypeVar
+
+from discord import ApplicationContext
 
 import database
 from message_formatting.embeds import EmbedBuilder
 from util.logger import log
 
+LimitedCommandParams = ParamSpec("LimitedCommandParams")
+LimitedCommandReturnValue = TypeVar("LimitedCommandReturnValue")
 
-def limit(_limit_level: int) -> callable:
+
+def limit(
+    limit_level_requirement: int,  # users at this `limitLevel` or higher are banned from using the command
+) -> Callable[
+    [Callable[LimitedCommandParams, Awaitable[LimitedCommandReturnValue]]],
+    Callable[LimitedCommandParams, Awaitable[LimitedCommandReturnValue | None]],
+]:
     """
+    ! This decorator only works on slash commands. !
     Limit levels:
     None: No limit
     1: Cannot use database commands (I.e alerts & applications)
@@ -15,54 +27,59 @@ def limit(_limit_level: int) -> callable:
     Levels are inclusive, so if you are level 2, you cannot use level 1 commands either.
     """
 
-    def decorator(func: callable) -> callable:
+    def decorator(
+        func: Callable[LimitedCommandParams, Awaitable[LimitedCommandReturnValue]],
+    ) -> Callable[LimitedCommandParams, Awaitable[LimitedCommandReturnValue | None]]:
         @functools.wraps(func)
-        async def wrapper(*args, **kwargs) -> callable:
+        async def wrapper(
+            *args: LimitedCommandParams.args,
+            **kwargs: LimitedCommandParams.kwargs,
+        ) -> LimitedCommandReturnValue | None:
             """
             Wrapper to determine if the user is limited or not.
             """
 
             # Get the author id
-            author_id = None
             for arg in args:
-                try:
-                    author_id = arg.author.id
+                if isinstance(arg, ApplicationContext):
                     ctx = arg
-                except AttributeError:  # Type is not a context
-                    continue
+                    break
+            else:
+                log(
+                    f"@limit() decorator was applied to non-slash-command {func!r}! "
+                    "Permitting function call without checking permissions!"
+                )
+                return await func(*args, **kwargs)
 
-            if (
-                author_id is not None
-            ):  # if no author exists, then we cannot limit, return func instead
-                conn = database.connect()
-                c = conn.cursor()
-                limit_level = c.execute(
-                    "SELECT limitLevel from user where uid = ?",
-                    (author_id,),
-                ).fetchone()
+            conn = database.connect()
+            c = conn.cursor()
+            limit_level = c.execute(
+                "SELECT limitLevel from user where uid = ?",
+                (ctx.author.id,),
+            ).fetchone()
 
-                if limit_level is None:  # User DOES NOT exist in database, add them.
-                    c.execute(
-                        "INSERT INTO user VALUES (?, ?, ?, ?, ?, ?)",
-                        (author_id, 0, False, None, 0, None),
-                    )  # See ERD.mdj
-                    limit_level = 0
-                    conn.commit()
-                else:
-                    limit_level = limit_level[0] or 0
+            if limit_level is None:  # User DOES NOT exist in database, add them.
+                c.execute(
+                    "INSERT INTO user VALUES (?, ?, ?, ?, ?, ?)",
+                    (ctx.author.id, 0, False, None, 0, None),
+                )  # See ERD.mdj
+                limit_level = 0
+                conn.commit()
+            else:
+                limit_level = limit_level[0] or 0
 
-                if limit_level >= _limit_level:
-                    embed = EmbedBuilder(
-                        title="You cannot use this command!",
-                        description="You are limited from using this command! Please contact a moderator if you believe this is a mistake.",
-                        color=0xFF0000,
-                    ).build()
-                    await ctx.respond(embed=embed, ephemeral=True)
-                    log(
-                        f"$ tried to use {ctx.command.name}. But is limited from using it.",
-                        ctx.author,
-                    )
-                    return None
+            if limit_level >= limit_level_requirement:
+                embed = EmbedBuilder(
+                    title="You cannot use this command!",
+                    description="You are limited from using this command! Please contact a moderator if you believe this is a mistake.",
+                    color=0xFF0000,
+                ).build()
+                await ctx.respond(embed=embed, ephemeral=True)
+                log(
+                    f"$ tried to use {ctx.command.name}. But is limited from using it.",
+                    ctx.author,
+                )
+                return None
 
             return await func(*args, **kwargs)
 
