481040ab2f289f96a191c3cae4d9eddbff6d616a
Dan <dparizher@gmail.com>
2024-06-23 15:52:25 -0400

    revert change

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix="g.", intents=discord_intents)
 def load() -> None:
     cogs = [
         "alerts",
-        # "deepl",
+        "deepl",
         "detect_ai",
         "detect_code",
         "detect_media_spam",
